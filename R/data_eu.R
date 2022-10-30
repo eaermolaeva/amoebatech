96673f7f81c8b0ea269e039c885a17e830a5e98f
ElizavetaErmolaeva <ermolaevalisha@yandex.com>
2022-10-30 14:12:36 +0100

    commit3

---
+++ R/data_eu.R
@@ -0,0 +1,17 @@
+#' Data of testate amoebae samples from Amesbury at al., 2016 
+#'
+#' Testate amoebae species are pooled in larger groups, environmental data includes WTD and pH variables
+#'
+#' @format A tibble with 1302 rows and 55 variables:
+#' \describe{
+#'   \item{person}{}
+#'   \item{country}{} 
+#'   \item{site}{}
+#'   \item{sample}{}
+#'   \item{lat}{}
+#'   \item{long}{}
+#'   \item{wtd}{}
+#'   \item{ph}{}
+#' }
+#' @source \url{https://www.sciencedirect.com/science/article/pii/S0277379116303742}
+"data_eu"
