e828402a587b88b0595f615909ae9d22ee5f5730
ElizavetaErmolaeva <ermolaevalisa8@gmail.com>
2022-11-27 23:57:41 +0100

    fixing third typo adding spaces

--- data-raw/data_eu_load.R
+++ data-raw/data_eu_load.R
@@ -4,14 +4,21 @@ library(tidyverse)
 
 data_eu <- 
   readr::read_csv("data-raw/data_eu.csv") %>%
-  #lowercase all the columns and replace spaces with underscore
-  rename_with(., ~ tolower(gsub(" ", "_", .x, fixed = TRUE))) %>%
+  
+  #lowercase all the columns and replace spaces with dots
+  rename_with(., ~ tolower(gsub(" ", ".", .x, fixed = TRUE))) %>%
+  
   #uppercase species groups names (optional)
-  #rename_with(., toupper, contains("_")) %>%
+  #rename_with(., toupper, contains(".")) %>%
+  
   #rename typos occured in the origibal dataset
-  rename(phy_gri = psy_gri, pla_car = neb_car) %>%
+  rename(phy.gri = psy.gri, 
+         pla.car = neb.car,
+         gib.pen = neb.pen) %>%
+  
   #order species groups alphabetically
-  select(person, country, site, sample, lat, long, wtd, ph, sort(tidyselect::peek_vars()))
+  select(person, country, site, sample, lat, long, wtd, ph, 
+         sort(tidyselect::peek_vars()))
 
 # export the data as an .rda file to the data/ folder
 
